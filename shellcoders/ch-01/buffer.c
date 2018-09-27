eb288282c17942063337e4215283b1b2e3dd5031
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-09-26 21:35:30 -0500

    Add shellcoders's examples for ch01 and ch02
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

---
+++ shellcoders/ch-01/buffer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+
+    printf("%d\n", array[5]);
+}
---
+++ shellcoders/ch-02/function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void function(int a, int c) {
+    int array[5];
+}
+
+int main() {
+    function(1, 2);
+    printf("This is where the return address points\n");
+}
